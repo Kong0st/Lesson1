321e7079705d9cede0f5d6f214a216cad467da74
Kong0st <kongost@yandex.ru>
2024-05-28 12:25:22 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name_of_course = 'Python, '
+do_DZ = 'всего задач '
+number_DZ = 12
+name = 'затрачено часов'
+number_hours = 1.5
+middle_time = 'среднее время выполнения'
+time_for_1_task = (number_hours/number_DZ)
+print ("Курс: ", name_of_course, do_DZ, number_DZ, ", ", name, number_hours, ", ", middle_time, time_for_1_task, 'часа.')
